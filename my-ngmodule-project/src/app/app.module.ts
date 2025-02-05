cdf2f7f61847e9c0c91dfb047661112eb6b99ba7
arafaseed <arafaseed2000@gmail.com>
2025-02-05 13:21:55 +0300

    arafa

--- my-ngmodule-project/src/app/app.module.ts
+++ my-ngmodule-project/src/app/app.module.ts
@@ -5,8 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { HomeComponent } from './components/home/home.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { VenueComponent } from './components/venue/venue.component';
@@ -21,9 +20,8 @@ import { VenueComponent } from './components/venue/venue.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule,
     HeaderComponent,
-    MatButtonModule,
+ 
   ],
   providers: [
     provideAnimationsAsync()
