9368a09555b1a0b74d12e73611153172c0ec81cc
Rio-OMR21 <orinaryaga21@gmail.com>
2025-05-26 00:56:47 +0300

    Updated staff home component not to appear twice

--- my-ngmodule-project/src/app/staff/staff-routing.module.ts
+++ my-ngmodule-project/src/app/staff/staff-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StaffDashboardComponent } from './staff-dashboard/staff-dashboard.component';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: StaffDashboardComponent,
-    children: [
-      { path: '', component: HomeComponent },
-    ]
   }
 ];
 
--- my-ngmodule-project/tailwind.config.js
+++ my-ngmodule-project/tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
       "./projects/**/*.{html,ts}"
     ],
     theme: {
+      colors: {
+      // Replace oklch with RGB/HEX equivalents
+      primary: '#4F46E5', // Example replacement
+      secondary: 'rgb(239 68 68)'
+    },
       extend: {},
     },
     plugins: [],
