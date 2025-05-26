37751494e215bfc53c9ed9bd31e06a64c8b24064
mella <mellabae322@gmail.com>
2025-05-26 15:56:46 +0300

    staff add successfully

--- my-ngmodule-project/src/app/Services/staff-view.service.ts
+++ my-ngmodule-project/src/app/Services/staff-view.service.ts
@@ -1,32 +1,43 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StaffService {
-  private apiUrl = 'http://localhost:8080/api/staff'; // base URL of your backend
+export class StaffViewService {
+  private apiUrl = 'http://localhost:8080/api/staff';
 
   constructor(private http: HttpClient) {}
 
-  // Fetch all staff
-  getAllStaff(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/all`);
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token'); // Or however you store your auth token
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
   }
 
-  // Add a new staff member
+  getAllStaff(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/all`, {
+      headers: this.getAuthHeaders()
+    });
+  }
+
   addStaff(staffData: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/add`, staffData);
+    return this.http.post(`${this.apiUrl}/add`, staffData, {
+      headers: this.getAuthHeaders()
+    });
   }
 
-  // Optionally: Delete a staff member by IDN
-  deleteStaff(staffIDN: string): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/delete/${staffIDN}`);
+  updateStaff(staffId: number, updatedData: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/update/${staffId}`, updatedData, {
+      headers: this.getAuthHeaders()
+    });
   }
 
-  // Optionally: Update a staff member
-  updateStaff(staffIDN: string, updatedData: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/update/${staffIDN}`, updatedData);
+  deleteStaff(staffId: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/delete/${staffId}`, {
+      headers: this.getAuthHeaders()
+    });
   }
 }
