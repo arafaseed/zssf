8c2a0e773a8c8da6917556a47140ade976f6ce6c
UmmulAmin <ummylmutwafy01@gmail.com>
2025-11-19 16:28:39 +0300

    Environment

--- my-ngmodule-project/src/environments/environment.ts
+++ my-ngmodule-project/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
 
-  // apiUrl: 'http://192.168.2.4:6070',
+ // apiUrl: 'http://192.168.2.4:6070',
   apiUrl: 'http://localhost:6070',
 
-
+ 
     production: false,
 
 };
